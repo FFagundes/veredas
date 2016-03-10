664f9dbad2353f9f198725a6982b4cdc11bbec81
william <brshimaoka@gmail.com>
2016-03-10 12:34:38 -0300

    Galeria de Salas - Sem conteúdo.

---
+++ js/legaleria.js
@@ -0,0 +1,111 @@
+
+
+document.getElementById('thumb-sala-a-01').onclick = function () {
+    document.getElementById('sala-a-01').className = 'gallery-img on';
+    document.getElementById('sala-a-02').className = 'gallery-img off';
+    document.getElementById('sala-a-03').className = 'gallery-img off';
+    document.getElementById('sala-a-04').className = 'gallery-img off';
+    document.getElementById('sala-a-05').className = 'gallery-img off';
+};
+document.getElementById('thumb-sala-a-02').onclick = function () {
+    document.getElementById('sala-a-01').className = 'gallery-img off';
+    document.getElementById('sala-a-02').className = 'gallery-img on';
+    document.getElementById('sala-a-03').className = 'gallery-img off';
+    document.getElementById('sala-a-04').className = 'gallery-img off';
+    document.getElementById('sala-a-05').className = 'gallery-img off';
+};
+document.getElementById('thumb-sala-a-03').onclick = function () {
+    document.getElementById('sala-a-01').className = 'gallery-img off';
+    document.getElementById('sala-a-02').className = 'gallery-img off';
+    document.getElementById('sala-a-03').className = 'gallery-img on';
+    document.getElementById('sala-a-04').className = 'gallery-img off';
+    document.getElementById('sala-a-05').className = 'gallery-img off';
+};
+document.getElementById('thumb-sala-a-04').onclick = function () {
+    document.getElementById('sala-a-01').className = 'gallery-img off';
+    document.getElementById('sala-a-02').className = 'gallery-img off';
+    document.getElementById('sala-a-03').className = 'gallery-img off';
+    document.getElementById('sala-a-04').className = 'gallery-img on';
+    document.getElementById('sala-a-05').className = 'gallery-img off';
+};
+document.getElementById('thumb-sala-a-05').onclick = function () {
+    document.getElementById('sala-a-01').className = 'gallery-img off';
+    document.getElementById('sala-a-02').className = 'gallery-img off';
+    document.getElementById('sala-a-03').className = 'gallery-img off';
+    document.getElementById('sala-a-04').className = 'gallery-img off';
+    document.getElementById('sala-a-05').className = 'gallery-img on';
+};
+
+
+document.getElementById('thumb-sala-b-01').onclick = function () {
+    document.getElementById('sala-b-01').className = 'gallery-img on';
+    document.getElementById('sala-b-02').className = 'gallery-img off';
+    document.getElementById('sala-b-03').className = 'gallery-img off';
+    document.getElementById('sala-b-04').className = 'gallery-img off';
+    document.getElementById('sala-b-05').className = 'gallery-img off';
+};
+document.getElementById('thumb-sala-b-02').onclick = function () {
+    document.getElementById('sala-b-01').className = 'gallery-img off';
+    document.getElementById('sala-b-02').className = 'gallery-img on';
+    document.getElementById('sala-b-03').className = 'gallery-img off';
+    document.getElementById('sala-b-04').className = 'gallery-img off';
+    document.getElementById('sala-b-05').className = 'gallery-img off';
+};
+document.getElementById('thumb-sala-b-03').onclick = function () {
+    document.getElementById('sala-b-01').className = 'gallery-img off';
+    document.getElementById('sala-b-02').className = 'gallery-img off';
+    document.getElementById('sala-b-03').className = 'gallery-img on';
+    document.getElementById('sala-b-04').className = 'gallery-img off';
+    document.getElementById('sala-b-05').className = 'gallery-img off';
+};
+document.getElementById('thumb-sala-b-04').onclick = function () {
+    document.getElementById('sala-b-01').className = 'gallery-img off';
+    document.getElementById('sala-b-02').className = 'gallery-img off';
+    document.getElementById('sala-b-03').className = 'gallery-img off';
+    document.getElementById('sala-b-04').className = 'gallery-img on';
+    document.getElementById('sala-b-05').className = 'gallery-img off';
+};
+document.getElementById('thumb-sala-b-05').onclick = function () {
+    document.getElementById('sala-b-01').className = 'gallery-img off';
+    document.getElementById('sala-b-02').className = 'gallery-img off';
+    document.getElementById('sala-b-03').className = 'gallery-img off';
+    document.getElementById('sala-b-04').className = 'gallery-img off';
+    document.getElementById('sala-b-05').className = 'gallery-img on';
+};
+
+
+document.getElementById('thumb-sala-c-01').onclick = function () {
+    document.getElementById('sala-c-01').className = 'gallery-img on';
+    document.getElementById('sala-c-02').className = 'gallery-img off';
+    document.getElementById('sala-c-03').className = 'gallery-img off';
+    document.getElementById('sala-c-04').className = 'gallery-img off';
+    document.getElementById('sala-c-05').className = 'gallery-img off';
+};
+document.getElementById('thumb-sala-c-02').onclick = function () {
+    document.getElementById('sala-c-01').className = 'gallery-img off';
+    document.getElementById('sala-c-02').className = 'gallery-img on';
+    document.getElementById('sala-c-03').className = 'gallery-img off';
+    document.getElementById('sala-c-04').className = 'gallery-img off';
+    document.getElementById('sala-c-05').className = 'gallery-img off';
+};
+document.getElementById('thumb-sala-c-03').onclick = function () {
+    document.getElementById('sala-c-01').className = 'gallery-img off';
+    document.getElementById('sala-c-02').className = 'gallery-img off';
+    document.getElementById('sala-c-03').className = 'gallery-img on';
+    document.getElementById('sala-c-04').className = 'gallery-img off';
+    document.getElementById('sala-c-05').className = 'gallery-img off';
+};
+document.getElementById('thumb-sala-c-04').onclick = function () {
+    document.getElementById('sala-c-01').className = 'gallery-img off';
+    document.getElementById('sala-c-02').className = 'gallery-img off';
+    document.getElementById('sala-c-03').className = 'gallery-img off';
+    document.getElementById('sala-c-04').className = 'gallery-img on';
+    document.getElementById('sala-c-05').className = 'gallery-img off';
+};
+document.getElementById('thumb-sala-c-05').onclick = function () {
+    document.getElementById('sala-c-01').className = 'gallery-img off';
+    document.getElementById('sala-c-02').className = 'gallery-img off';
+    document.getElementById('sala-c-03').className = 'gallery-img off';
+    document.getElementById('sala-c-04').className = 'gallery-img off';
+    document.getElementById('sala-c-05').className = 'gallery-img on';
+};
